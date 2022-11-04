099115298a005e569e6701e75c8fcf535566504b
Idrusbasha <idrusbasha278@gmail.com>
2022-11-04 19:17:36 +0530

    first commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/youtubeRegistration", {
+mongoose.connect("mongodb+srv://idrusbasha:basha@cluster0.9rih3zh.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
