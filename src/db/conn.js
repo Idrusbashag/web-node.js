ef76f216195d36beed3ccd68f02e57879b5d543e
Idrusbasha <idrusbasha211@gmail.com>
2022-11-07 15:38:18 +0530

    first commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://idrusbasha:basha@cluster0.xp7pfy6.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
+
 }).then(() => {
     console.log(`connection successful`);
 }).catch((e) => {
