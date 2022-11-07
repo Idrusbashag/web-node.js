d0fe6baf2c89623d6d01d21c5318ab40b0bb3883
Idrusbasha <idrusbasha211@gmail.com>
2022-11-07 16:13:32 +0530

    first commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://idrusbasha:basha@cluster0.xp7pfy6.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://idrusbasha:basha@ac-dkqtsux-shard-00-00.xp7pfy6.mongodb.net:27017,ac-dkqtsux-shard-00-01.xp7pfy6.mongodb.net:27017,ac-dkqtsux-shard-00-02.xp7pfy6.mongodb.net:27017/?ssl=true&replicaSet=atlas-954rpz-shard-0&authSource=admin&retryWrites=true&w=majority", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
 
 }).then(() => {
     console.log(`connection successful`);
