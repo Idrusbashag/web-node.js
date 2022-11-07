8979bc3d6d904d733e031b7e4fb060bb3913d158
Idrusbasha <idrusbasha278@gmail.com>
2022-11-07 11:45:06 +0530

    heroku website hosting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const cors =require("cors")
 const app = express();
+app.use(cors());
 const hbs = require("hbs");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://idrusbasha:basha@cluster0.9rih3zh.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
+    
 }).then(() => {
     console.log(`connection successful`);
 }).catch((e) => {
